420063dfe66bcc1ecb14acd8f66c98333e328bbb
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-02-16 16:13:26 -0300

    add js

--- script.js
+++ script.js
@@ -33,3 +33,51 @@ var categories = [
     { id: 2, name: "Nacional" },
     { id: 3, name: "Importado" }
 ];
+
+//Carregamento
+loadProducts();
+
+//carregar todos os produtos do array
+function loadProducts(){
+    for(let prod of products){
+        addNewRow(prod);
+        
+    }
+
+}
+
+//add nova linha
+ function addNewRow(prod){
+    var table = document.getElementById("productsTable");
+
+    var newRow = table.insertRow();
+
+
+    //inserir id do produto
+    var idNode = document.createTextNode(prod.id);
+    newRow.insertCell().appendChild(idNode);
+    //inserir nome do produto
+    var name = document.createTextNode(prod.name);
+    newRow.insertCell().appendChild(name);
+    //inserir descricao do produto
+    var desc = document.createTextNode(prod.description);
+    newRow.insertCell().appendChild(desc);
+    //inserir preco do produto
+    var price = document.createTextNode(prod.price);
+    newRow.insertCell().appendChild(price);
+    //inserir categoria
+    var cat = document.createTextNode(categories[prod.category-1].name);
+    newRow.insertCell().appendChild(cat);
+
+    var options='';
+
+    if(prod.promotion){
+        options = '<span class="badge bg-success me-2">P</span>'
+    }
+    if(prod.new){
+        options += '<span class="badge bg-secondary ">L</span>'
+    }
+
+    newRow.insertCell().innerHTML = options;
+
+}
