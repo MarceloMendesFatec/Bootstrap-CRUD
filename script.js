c67eaba16bfecc3eca52c3af4e2f59aabbff393d
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-02-16 15:16:38 -0300

    formatacao do projeto

---
+++ script.js
@@ -0,0 +1,35 @@
+var products = [
+    {
+        id: 1,
+        name: "Computador M1-TX",
+        description: "Intel I7, 16GB, SSD 256, HD 1T",
+        price: 4900,
+        category: 1,
+        promotion: true,
+        new: true
+    },
+    {
+        id: 2,
+        name: "Computador M2-TX",
+        description: "Intel I7, 32GB, SSD 512, HD 1T",
+        price: 5900,
+        category: 2,
+        promotion: false,
+        new: true
+    },
+    {
+        id: 3,
+        name: "Computador M1-T",
+        description: "Intel I5, 16GB, HD 1T",
+        price: 2900,
+        category: 3,
+        promotion: false,
+        new: false
+    },
+];
+
+var categories = [
+    { id: 1, name: "Produção Própria" },
+    { id: 2, name: "Nacional" },
+    { id: 3, name: "Importado" }
+];
